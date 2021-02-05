9bfd960d89e6caf929138c68eb46e5b0e04f82a0
Shahriar Momen <66987174+shahriar-swe@users.noreply.github.com>
2021-02-04 21:30:12 -0800

    1st Commit

---
+++ javaapplication1/JavaApplication2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author shahr
+ */
+public class JavaApplication2 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+    
+}
